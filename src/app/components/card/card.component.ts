229f2271099fa1b3b41a0a6cfa1ad869dab189f8
Aldo Santos <aldoacesso@gmail.com>
2023-10-05 15:23:51 -0300

    Preparando o consumo da poke API

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  name: string = "CHARIZARD"
+  attributesTypes: string[] = ['FIRE', 'ROCK']
+
+  constructor(
+    private service: PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    this.service.getPokemon("charizard").subscribe(
+      {
+        next: (res) => console.log(res),
+        error: (err) => console.log(err)
+      }
+    )
+  }
+}
